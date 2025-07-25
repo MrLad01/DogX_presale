bdf7905fe0a550e857a131069ca7141d71e9aa15
mrlad01 <ladayodeji@gmail.com>
2025-07-25 13:30:41 +0100

    update

--- programs/presalee/src/instructions/buy_token.rs
+++ programs/presalee/src/instructions/buy_token.rs
@@ -1,10 +1,20 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token::{Token, TokenAccount};
+
+use crate::state::Presale;
 
 #[derive(Accounts)]
  pub struct BuyToken <'info> {
     #[account(mut)]
     pub buyer: Signer<'info>,
+    #[account(mut)]
+    pub presale: Account<'info, Presale>,
+    #[account(mut)]
+    pub buyer_mint: Account<'info, TokenAccount>,
+    #[account(mut)]
+    pub presale_mint: Account<'info, TokenAccount>,
 
-    
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>
 
  }
